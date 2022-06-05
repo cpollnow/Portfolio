7c474d07b6a1b05811d33e54c105c7e074b3e235
cpollnow <101438040+cpollnow@users.noreply.github.com>
2022-06-05 19:36:40 -0400

    Update Flights - Departure Delays by Airport.sql

--- Flights - Departure Delays by Airport.sql
+++ Flights - Departure Delays by Airport.sql
@@ -1,10 +1,10 @@
-/*CASE statement to determine whether a flight is early, on-time, late.*/
+/*Query to establish which airports had a high frequency of late departures.*/
 
 USE Flights
 GO
 SELECT COUNT(AirportID) AS Airport_Delay_Count, AirportID
 FROM
-	(SELECT Top 1000 AVG(DepDelay) AS Departure_Delay, Carrier AS Airline, AirportID
+	(SELECT Top 100 AVG(DepDelay) AS Departure_Delay, Carrier AS Airline, AirportID
 	FROM Flights
 	GROUP BY Carrier, AirportID
 	ORDER BY Departure_Delay DESC) AS Subquery
