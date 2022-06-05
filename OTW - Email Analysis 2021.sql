9bea99830df23fdfe477d1d56c0c5627f05a5a1f
cpollnow <101438040+cpollnow@users.noreply.github.com>
2022-06-05 19:41:37 -0400

    Update OTW - Email Analysis 2021.sql

--- OTW - Email Analysis 2021.sql
+++ OTW - Email Analysis 2021.sql
@@ -1,16 +1,18 @@
 USE [On The Water]
 GO
 
+/*Query to establish the top 15 unsubscribe email subject lines.*/
 SELECT Top 15 Subject AS Email_Subject_Line, Open_Rate, Unique_Opens, Click_Rate, Unsubscribes 
 FROM MailChimp
 ORDER BY Unsubscribes DESC
 
-
+/*Query to perform analysis in regards to 'trout' being in the subject line.*/
 SELECT Subject AS Email_Subject_Line, Open_Rate, Unique_Opens, Click_Rate, Unsubscribes 
 FROM MailChimp
 WHERE Subject LIKE '%trout%'
 ORDER BY Unsubscribes DESC
 
+*/Query to determine the average number of unsubscribes*/
 SELECT AVG(Unsubscribes) AS Avg_Unsubscribes
 FROM MailChimp AS MC
 
